e6467b5c67384d680657aa53a6d20719233bfba4
Shigurefox <shigurefox@gmail.com>
2020-01-30 14:21:27 +0800

    Add files via upload

---
+++ my-mod3.h
@@ -0,0 +1,11 @@
+#include <stdint.h>
+
+void polyf3_pack_fast(uint32_t *, uint32_t *);
+void polyf3_unpack_fast(uint32_t *, uint32_t *);
+void polyf3_add_packed_fast(uint32_t *, uint32_t *, uint32_t *);
+void polyf3_sub_packed_fast(uint32_t *, uint32_t *, uint32_t *);
+void polyf3_mul_packed_fast(uint32_t *, uint32_t *, uint32_t *);
+
+void polyf3_mul_scalar_packed_fast(uint32_t *, uint32_t *, uint32_t *);
+void polyf3_divstep(int32_t, uint32_t *, uint32_t *, uint32_t **);
+void polyf3_jump32divsteps(int32_t, uint32_t *, uint32_t *, uint32_t **);
