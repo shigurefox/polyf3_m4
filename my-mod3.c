a13960190a58933839e53ddbfd41278e3a4210bd
Shigurefox <shigurefox@gmail.com>
2020-02-13 16:36:25 +0800

    add rol32, rol64 w/ asm, butterfly32/64

--- my-mod3.c
+++ my-mod3.c
@@ -1418,20 +1418,20 @@ void polyf3_butterfly32_CT(uint32_t *tritIn1, uint32_t *tritIn2, int k) {
     r2 = *(readOp2++);
     r3 = *(readOp2++);
 
-    r6 = r1 ^ r2; // r6 = a1 ^ b0
-    r5 = r6 ^ r3; // r5 = (a1 ^ b0) ^ b1
-    r7 = r0 ^ r2; // r7 = a0 ^ b0
-    r5 = r5 ^ r7; // r5 = ((a1 ^ b0) ^ b1) | (a0 ^ b0)
+    r8 = r1 ^ r2; // r8 = a1 ^ b0
+    r5 = r8 ^ r3; // r5 = (a1 ^ b0) ^ b1
+    r9 = r0 ^ r2; // r9 = a0 ^ b0
     r4 = r0 ^ r3; // r4 = a0 ^ b1
-    r4 = r4 & r6; // r4 = (a0 ^ b1) & (a1 ^ b0)
-    r8 = r5 & r6; // r8 = (a1 ^ b0 ^ b1) & (a1 ^ b0)
-    r9 = r1 ^ r3; // r9 = a1 ^ b1
-    r9 = r9 | r6; // r9 = (a0 ^ b0) | (a1 ^ b1)
+    r4 = r4 & r8; // r4 = (a0 ^ b1) & (a1 ^ b0)
+    r6 = r5 & r8; // r6 = (a1 ^ b0 ^ b1) & (a1 ^ b0)
+    r5 = r5 | r9; // r5 = ((a1 ^ b0) ^ b1) | (a0 ^ b0)
+    r7 = r1 ^ r3; // r7 = a1 ^ b1
+    r7 = r7 | r9; // r7 = (a0 ^ b0) | (a1 ^ b1)
 
     *(writeOp1++) = r4;
     *(writeOp1++) = r5;
-    *(writeOp2++) = r8;
-    *(writeOp2++) = r9;
+    *(writeOp2++) = r6;
+    *(writeOp2++) = r7;
 }
 
 void polyf3_butterfly32_GS(uint32_t *tritIn1, uint32_t *tritIn2, int k) {
@@ -1443,15 +1443,15 @@ void polyf3_butterfly32_GS(uint32_t *tritIn1, uint32_t *tritIn2, int k) {
     r2 = *(readOp2++);
     r3 = *(readOp2++);
 
-    r6 = r1 ^ r2; // r6 = a1 ^ b0
-    r5 = r6 ^ r3; // r5 = (a1 ^ b0) ^ b1
-    r7 = r0 ^ r2; // r7 = a0 ^ b0
-    r5 = r5 ^ r7; // r5 = ((a1 ^ b0) ^ b1) | (a0 ^ b0)
+    r8 = r1 ^ r2; // r8 = a1 ^ b0
+    r5 = r8 ^ r3; // r5 = (a1 ^ b0) ^ b1
+    r9 = r0 ^ r2; // r9 = a0 ^ b0
     r4 = r0 ^ r3; // r4 = a0 ^ b1
-    r4 = r4 & r6; // r4 = (a0 ^ b1) & (a1 ^ b0)
-    r8 = r5 & r6; // r8 = (a1 ^ b0 ^ b1) & (a1 ^ b0)
-    r9 = r1 ^ r3; // r9 = a1 ^ b1
-    r9 = r9 | r6; // r9 = (a0 ^ b0) | (a1 ^ b1)
+    r4 = r4 & r8; // r4 = (a0 ^ b1) & (a1 ^ b0)
+    r6 = r5 & r8; // r6 = (a1 ^ b0 ^ b1) & (a1 ^ b0)
+    r5 = r5 | r9; // r5 = ((a1 ^ b0) ^ b1) | (a0 ^ b0)
+    r7 = r1 ^ r3; // r7 = a1 ^ b1
+    r7 = r7 | r9; // r7 = (a0 ^ b0) | (a1 ^ b1)
 
     *(writeOp1++) = r4;
     *(writeOp1++) = r5;
@@ -1461,9 +1461,60 @@ void polyf3_butterfly32_GS(uint32_t *tritIn1, uint32_t *tritIn2, int k) {
     polyf3_rol32_negc(tritIn2, k);
 }
 
-void polyf3_butterfly64_CT(uint32_t *, uint32_t *, int k) {
+void polyf3_butterfly64_CT(uint32_t *tritIn1, uint32_t *tritIn2, int k) {
+    uint32_t r0, r1, r2, r3, r4, r5, r6, r7, r8, r9;
+    uint32_t *readOp1 = tritIn1, *readOp2 = tritIn2, *writeOp1 = tritIn1, *writeOp2 = tritIn2;
 
+    polyf3_rol64_negc(tritIn2, k);
+
+    for (int i = 0; i < 2; i++) {
+        r0 = *(readOp1++);
+        r1 = *(readOp1++);
+        r2 = *(readOp2++);
+        r3 = *(readOp2++);
+
+        r8 = r1 ^ r2; // r8 = a1 ^ b0
+        r5 = r8 ^ r3; // r5 = (a1 ^ b0) ^ b1
+        r9 = r0 ^ r2; // r9 = a0 ^ b0
+        r4 = r0 ^ r3; // r4 = a0 ^ b1
+        r4 = r4 & r8; // r4 = (a0 ^ b1) & (a1 ^ b0)
+        r6 = r5 & r8; // r6 = ((a1 ^ b0) ^ b1) & (a1 ^ b0)
+        r5 = r5 | r9; // r5 = ((a1 ^ b0) ^ b1) | (a0 ^ b0)
+        r7 = r1 ^ r3; // r7 = a1 ^ b1
+        r7 = r7 | r9; // r7 = (a0 ^ b0) | (a1 ^ b1)
+
+        *(writeOp1++) = r4;
+        *(writeOp1++) = r5;
+        *(writeOp2++) = r6;
+        *(writeOp2++) = r7;
+    }
 }
-void polyf3_butterfly64_GS(uint32_t *, uint32_t *, int k) {
 
+void polyf3_butterfly64_GS(uint32_t *, uint32_t *, int k) {
+    uint32_t r0, r1, r2, r3, r4, r5, r6, r7, r8, r9;
+    uint32_t *readOp1 = tritIn1, *readOp2 = tritIn2, *writeOp1 = tritIn1, *writeOp2 = tritIn2;
+
+    for (int i = 0; i < 2; i++) {
+        r0 = *(readOp1++);
+        r1 = *(readOp1++);
+        r2 = *(readOp2++);
+        r3 = *(readOp2++);
+
+        r8 = r1 ^ r2; // r8 = a1 ^ b0
+        r5 = r8 ^ r3; // r5 = (a1 ^ b0) ^ b1
+        r9 = r0 ^ r2; // r9 = a0 ^ b0
+        r4 = r0 ^ r3; // r4 = a0 ^ b1
+        r4 = r4 & r8; // r4 = (a0 ^ b1) & (a1 ^ b0)
+        r6 = r5 & r8; // r6 = ((a1 ^ b0) ^ b1) & (a1 ^ b0)
+        r5 = r5 | r9; // r5 = ((a1 ^ b0) ^ b1) | (a0 ^ b0)
+        r7 = r1 ^ r3; // r7 = a1 ^ b1
+        r7 = r7 | r9; // r7 = (a0 ^ b0) | (a1 ^ b1)
+
+        *(writeOp1++) = r4;
+        *(writeOp1++) = r5;
+        *(writeOp2++) = r6;
+        *(writeOp2++) = r7;
+    }
+
+    polyf3_rol64_negc(tritIn2, k);
 }
